e347532d76aa98f70a5b14dbc3102e1456624a60
paripontt <paripontt@gmail.com>
2021-08-26 15:56:10 -0500

    initial commit

---
+++ exchange_etl.py
@@ -0,0 +1,15 @@
+#%%
+import datetime
+import logging
+import sys
+import requests
+
+url = 'https://api.coincap.io/v2/exchanges'
+r = requests.get(url)
+data = r.json().get('data',[])
+print(data)
+# %%
+import pandas as pd
+dataf = pd.DataFrame(data)
+datafp
+# %%
